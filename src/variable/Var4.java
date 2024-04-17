62472e3f3c8700de7a96a7c643d4e56549d7b8be
huiju0502 <yhj960502@naver.com>
2024-04-17 15:46:54 +0900

    2. 변수

---
+++ src/variable/Var4.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        // 변수는 하나씩 선언 할 수도 있고
+        int a;
+        int b;
+        // 한번에 여러 변수를 선언할 수도 있다
+        int c,d;
+    }
+}
---
+++ src/variable/Var6.java
@@ -0,0 +1,9 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+        // System.out.println(a);
+        // 변수가 초기화 되지 않았다는 오류 발생
+    }
+}
